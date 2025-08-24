c8ab94cee0bf8ee0a2ba4dbc4f40760fd6257996
András Tóth <andraas.toth@gmail.com>
2025-08-24 18:27:41 +0200

    Refactor

--- index.ts
+++ index.ts
@@ -1,21 +1,28 @@
-const playerNameInput = <HTMLInputElement>document.getElementById('playerName');
-const playButton = <HTMLButtonElement>document.getElementById('playButton');
+const playerNameInput = <HTMLInputElement>getById('playerName');
+const playButton = <HTMLButtonElement>getById('playButton');
 
-playerNameInput.addEventListener('blur', () => {
+playerNameInput.addEventListener('blur', onPlayerNameChange);
+
+getById('helloPlayerName').addEventListener('click', onPlayerNameClick);
+
+function onPlayerNameChange() {
 	const playerName = playerNameInput.value;
-	console.log(playerName);
 
 	if (playerName) {
 		playButton.disabled = false;
-		document.getElementById('helloPlayerName').innerText = playerName;
-		document.getElementById('helloSection').classList.add('show');
-		document.getElementById('inputSection').classList.remove('show');
+		getById('helloPlayerName').innerText = playerName;
+		getById('helloSection').classList.add('show');
+		getById('inputSection').classList.remove('show');
 	} else {
 		playButton.disabled = true;
 	}
-});
+}
 
-document.getElementById('helloPlayerName').addEventListener('click', () => {
-	document.getElementById('inputSection').classList.add('show');
-	document.getElementById('helloSection').classList.remove('show');
-});
+function onPlayerNameClick() {
+	getById('inputSection').classList.add('show');
+	getById('helloSection').classList.remove('show');
+}
+
+function getById(id: string) {
+	return document.getElementById(id);
+}
